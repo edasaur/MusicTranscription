c70df703ef225ffe1e496960cf96c29d0f7bc4b3
edasaur <edx.wang@gmail.com>
2014-10-04 06:46:11 -0700

    Added another library. Finished most of reading the audio file >_>

---
+++ makefile
@@ -0,0 +1,5 @@
+reader: reader.c
+	gcc -Wall reader.c -o reader
+
+clean:
+	rm -f reader
--- reader.c
+++ reader.c
@@ -1,41 +1,33 @@
-#include <studio.h>
+#include <stdio.h>
 #include <sndfile.h>
 
-main(const char *path) {
 
-    /** store the .wav file into arr **/
-    SNDFILE* arr = sf_open(cons char *path, int mode, SF_INFO *sfinfo);
-    // *path is the path to the file. Prob take as input from main
-    // mode is one of SFM_READ, SFM_WRITE, SFM_RDWR. We will use SFM_READ
-    // sf_info->format = 0 must be done before call to sf_open
-    // sndfile.h will fill in the rest of the fields in sf_info
-    // The exception to this is if you are working with a RAW file
-        // then you must specify sfinfo->samplerate, channels, format
-
-    if (arr == NULL) { // the function failed
-        printf(const char* sf_strerror(SNDFILE *arr);
+int main(const char *inputFilename) {
+    SNDFILE *input, *output;
+    SF_INFO sfinfo;
+    
+    //Following checks whether there is an error in opening and reading the audio file
+    if (! (input = sf_open(inputFilename, SFM_READ, &sfinfo))) {
+        printf("Error: File could not be opened: %s\n", inputFilename);
+        puts(sf_strerror(input)); //change back to NULL if 'input' does not work
+        exit(1);
     }
 
-    sf_count_t start = sf_seek(arr, sf_count_t frames, int whence);
-    // whence is one of SEEK_SET, SEEK_CUR, SEEK_END. We will use SEEK_SET
-    // SEEK_SET is the start of the audio data and frames is an additional
-    // offset == 0.
-    // Return value is not used
+    //set read pointer to beginning of audio data and returns position of read pointer, unless
+    //before beginning of file or after end of file, in which case, -1 is returned
+    sf_count_t offset = sf_seek(input, 0, SEEK_SET); 
+    //at this point, offset == 0
+    sf_count_t num_frames = 1024; // size of the array to be tossed into FFTW. Check if 1024 is valid
+        
+    while ((int)(sf_seek(input, 0, SEEK_CUR)) >= 0){
+        double interArray[num_frames];
+        offset = offset + sf_readf_double(input, input+(int)(offset), num_frames); //check the array to see if done correctly? Should be pointer pointing at current position in array
+        //probably want to do the FFT right here?
 
-    sf_count_t read, num_frames;
-    num_frames = N; // size of the array to be tossed into FFTW. Prob 1024?
-    
-    while (read == num_frames) {
-        read = sf_readf_type(arr, type *ptr, sf_count_t num_frames);
-        sf_seek(arr, read, SEEK_CUR);
-        // reads num_frames number of frames into *ptr
-        // *ptr contains either short, int, float, or double
-        // sf_seek(arr, frames, SEEK_CUR) moves the read pointer to the next
-        // section of the audio file that needs to be read
-        // READ is the actual number of frames that were read. Typically this
-        // should be == num_frames. If you reach the end of the file with 
-        // less than num_frames, READ will be the actual number read
-
-    int good = sf_close(arr);
-    // good == 0 if success else some error value
+        sf_seek(input, offset, SEEK_SET);
+    }
+    int good = sf_close(input);
+    if (sf_close(input)) {
+        printf("Successfully closed audio file!");
+    }
 }
